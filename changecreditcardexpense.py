0ba842848459bbc6b0d5be994275aaca7dace28f
uniz <nguyen_uniz@yahoo.ca>
2019-01-31 14:57:39 -0600

    use executemany for fast query

--- changecreditcardexpense.py
+++ changecreditcardexpense.py
@@ -1,59 +1,66 @@
 import pyodbc
 from decimal import *
 
+
 cn = pyodbc.connect('DSN=QuickBooks Data;',autocommit=True)
 cursor = cn.cursor()
-encoding = 'utf-8'
 
 sql = """SELECT TxnID, ExpenseLineMemo, ExpenseLineAmount, ExpenseLineCustomerRefFullName, ExpenseLineClassRefFullName, ExpenseLineTxnLineID 
 FROM CreditCardChargeExpenseLine UNOPTIMIZED
-WHERE TxnDate >= {d'2018-10-01'} AND ExpenseLineAccountRefFullName = 'Field Expenses'
-AND ExpenseLineCustomerRefFullName = 'XL Texas Lone Star Auction Lubbock' AND ExpenseLineAmount <> 0"""
-values = []
-
+WHERE TxnDate >= {d'2018-01-01'} AND ExpenseLineAccountRefFullName = 'Field Expenses'
+AND ExpenseLineCustomerRefFullName IN ('XL Greater Kalamazoo Auto Auction')
+AND ExpenseLineAmount <> 0"""
 cursor.execute(sql)
+
+values= []
+ExpenseLineTxnLineIDs = []
+errorlist = []
+ 
 for row in cursor.fetchall():
     values.append({'TxnID':row.TxnID,'ExpenseLineMemo':row.ExpenseLineMemo,'ExpenseLineAmount':row.ExpenseLineAmount,
     'ExpenseLineCustomerRefFullName':row.ExpenseLineCustomerRefFullName,
-    'ExpenseLineClassRefFullName':row.ExpenseLineClassRefFullName,'ExpenseLineTxnLineID':row.ExpenseLineTxnLineID})
+    'ExpenseLineClassRefFullName':row.ExpenseLineClassRefFullName})
+    ExpenseLineTxnLineIDs.append(row.ExpenseLineTxnLineID)
 
 cursor.close()
 
-
 for i in values:
-    
-    print (i)
-
-    cursor = cn.cursor()
     try:    
-        i['TxnID'] = i['TxnID'].encode(encoding)
-        i['ExpenseLineMemo'] = i['ExpenseLineMemo'].encode(encoding)
-        i['ItemLineItemRefFullName'] = str("MISC-EQUIP").encode(encoding)
+        i['TxnID'] = i['TxnID'].encode()
+        i['ExpenseLineMemo'] = i['ExpenseLineMemo'].encode()
+        i['ItemLineItemRefFullName'] = str("MISC-EQUIP").encode()
         i['ExpenseLineAmount'] = Decimal(i['ExpenseLineAmount'])
-        i['ExpenseLineCustomerRefFullName'] = i['ExpenseLineCustomerRefFullName'].encode(encoding)
-        i['ExpenseLineClassRefFullName'] = i['ExpenseLineClassRefFullName'].encode(encoding)
+        #i['ExpenseLineCustomerRefFullName'] = i['ExpenseLineCustomerRefFullName'].encode()
+        i['ExpenseLineCustomerRefFullName'] = str('XL Texas Lone Star Auction Lubbock:Equipment #1002058').encode()
+        i['ExpenseLineClassRefFullName'] = i['ExpenseLineClassRefFullName'].encode()
         i['ItemLineQuantity'] = Decimal(1)
-        i['BillStatus'] = str("NotBillable").encode(encoding)
-        i['ExpenseLineTxnLineID'] = i['ExpenseLineTxnLineID'].encode(encoding)
+        i['BillStatus'] = str("NotBillable").encode()
     except Exception as Error:
         print (Error,i['TxnID'])
+        errorlist.append(i['ExpenseLineAmount'])
         continue
 
-    sql1 = """INSERT INTO CreditCardChargeItemLine (TxnID, ItemLineItemRefFullName, ItemLineDesc, ItemLineQuantity, ItemLineAmount, ItemLineCustomerRefFullName, ItemLineBillableStatus,ItemLineClassRefFullName, FQSaveToCache)
+params = list(list(k.values()) for k in values)
+print (errorlist)
+
+
+ExpenseLineTxnLineIDs = tuple(str(i) for i in ExpenseLineTxnLineIDs)
+
+
+sql1 = """INSERT INTO CreditCardChargeItemLine (TxnID, ItemLineDesc,ItemLineAmount, ItemLineCustomerRefFullName, ItemLineClassRefFullName, ItemLineItemRefFullName,
+ItemLineQuantity, ItemLineBillableStatus, FQSaveToCache)
     VALUES (?,?,?,?,?,?,?,?,0)"""
 
-    sql2 = """UPDATE CreditCardChargeExpenseLine
+sql2 = f"""UPDATE CreditCardChargeExpenseLine
     SET ExpenseLineAmount = 0
-    WHERE ExpenseLineTxnLineID = ?"""
+    WHERE ExpenseLineTxnLineID IN {ExpenseLineTxnLineIDs}"""
 
-    cursor.execute(sql1,i['TxnID'],i['ItemLineItemRefFullName'],i['ExpenseLineMemo'],i['ItemLineQuantity'],i['ExpenseLineAmount'],i['ExpenseLineCustomerRefFullName'],i['BillStatus'],i['ExpenseLineClassRefFullName'])
-    cursor.execute(sql2,i['ExpenseLineTxnLineID'])
+cursor = cn.cursor()
+cn.autocommit = False
 
-    cursor.close()
+cursor.executemany(sql1,params)
+cursor.execute(sql2)
+cn.commit()
 
-
+cursor.close()
 cn.close()
-
-
-
-
